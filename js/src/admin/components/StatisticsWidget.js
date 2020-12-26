0a153c1b53200436956a64c31405bff598de6f11
skmedix <skmedixs@gmail.com>
2020-12-26 20:36:30 +0100

    :construction: Try parse int

--- js/src/admin/components/StatisticsWidget.js
+++ js/src/admin/components/StatisticsWidget.js
@@ -178,7 +178,7 @@ export default class StatisticsWidget extends DashboardWidget {
     for (const time in timed) {
       if (time >= period.start && time < period.end) {
         console.log({time: time, timed: timed[time], start: period.start, end: period.end});
-        count += timed[time];
+        count += parseInt(timed[time]);
       }
     }
 
